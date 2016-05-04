ab058f10a7f3d5d7d7778571ca178885c49ca4a6
Eric Liu <eric.liu.developer@gmail.com>
2016-05-04 22:38:11 +1000

    changes test set

--- src/test/java/com/ericliu/developer/sorting/SortingTest.java
+++ src/test/java/com/ericliu/developer/sorting/SortingTest.java
@@ -14,7 +14,7 @@ public class SortingTest {
 
     @Before
     public void setUp() throws Exception {
-        mArray = new int[]{4, 2, 6, 56, 57, 34, 2, 4, 0, 8, 2, 5};
+        mArray = new int[]{1, 3, 10, 5, 6, 4, 8, 9, 2, 7};
     }
 
     @After
