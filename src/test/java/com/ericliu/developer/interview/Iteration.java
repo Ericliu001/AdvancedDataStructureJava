e4f906cb9386547afcb7d1b6a60b0c4a49fe47cd
Eric Liu <eric.liu.developer@gmail.com>
2016-07-06 22:55:11 +1000

    adds comments

--- src/test/java/com/ericliu/developer/interview/Iteration.java
+++ src/test/java/com/ericliu/developer/interview/Iteration.java
@@ -4,6 +4,22 @@ import org.junit.Test;
 
 /**
  * Created by ericliu on 4/07/2016.
+ *
+ * # take an array and print non over lapping in order pairs. example:
+
+
+ # [1,2,3,4] => input
+
+ # output below is in order combination
+
+ # (1234)
+ # (1)(234)
+ # (1)(23)(4)
+ # (1)(2)(34)
+ # (12)(34)
+ # (12)(3)(4)
+ # (123)(4)
+ # (1)(2)(3)(4)
  */
 public class Iteration {
 
