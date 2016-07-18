cd7dc7110729deb2bd9fc9a44c54464aa2c162e6
Eric Liu <eric.liu.developer@gmail.com>
2016-07-18 17:06:34 +1000

    improves readability of merge sort example

--- src/main/java/com/ericliu/developer/sorting/MergeSort.java
+++ src/main/java/com/ericliu/developer/sorting/MergeSort.java
@@ -9,7 +9,7 @@ public class MergeSort extends Sort {
         if (array.length < 2) { // notice it's the array's length, not sub array's length
             return array;
         }
-        int subLength = array.length/2;
+        int subLength = array.length / 2;
 
         int[] left = new int[subLength];
         int[] right = new int[array.length - subLength]; // notice the length here
@@ -30,7 +30,7 @@ public class MergeSort extends Sort {
 
 
     public void merge(int[] left, int[] right, int[] merged) {
-        int i = 0, j = 0,  k = 0;
+        int i = 0, j = 0, k = 0;
 
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
@@ -59,70 +59,50 @@ public class MergeSort extends Sort {
     }
 
 
-
-
-    /**
-     * Tuning parameter: list size at or below which insertion sort will be
-     * used in preference to mergesort.
-     * To be removed in a future release.
-     */
-    private static final int INSERTIONSORT_THRESHOLD = 7;
-
-    /**
-     * Src is the source array that starts at index 0
-     * Dest is the (possibly larger) array destination with a possible offset
-     * low is the index in dest to start sorting
-     * high is the end index in dest to end sorting
-     * off is the offset to generate corresponding low, high in src
-     * To be removed in a future release.
-     */
-    private static void mergeSort(Object[] src,
-                                  Object[] dest,
-                                  int low,
-                                  int high,
-                                  int off) {
-        int length = high - low;
-
-        // Insertion sort on smallest arrays
-        if (length < INSERTIONSORT_THRESHOLD) {
-            for (int i=low; i<high; i++)
-                for (int j=i; j>low &&
-                        ((Comparable) dest[j-1]).compareTo(dest[j])>0; j--)
-                    swap(dest, j, j-1);
-            return;
-        }
-
-        // Recursively sort halves of dest into src
-        int destLow  = low;
-        int destHigh = high;
-        low  += off;
-        high += off;
-        int mid = (low + high) >>> 1;
-        mergeSort(dest, src, low, mid, -off);
-        mergeSort(dest, src, mid, high, -off);
-
-        // If list is already sorted, just copy from src to dest.  This is an
-        // optimization that results in faster sorts for nearly ordered lists.
-        if (((Comparable)src[mid-1]).compareTo(src[mid]) <= 0) {
-            System.arraycopy(src, low, dest, destLow, length);
-            return;
-        }
-
-        // Merge sorted halves (now in src) into dest
-        for(int i = destLow, p = low, q = mid; i < destHigh; i++) {
-            if (q >= high || p < mid && ((Comparable)src[p]).compareTo(src[q])<=0)
-                dest[i] = src[p++];
-            else
-                dest[i] = src[q++];
-        }
-    }
-
     /**
      * Swaps x[a] with x[b].
      */
-    private static void swap(Object[] x, int a, int b) {
-        Object t = x[a];
+    private static void swap(int[] x, int a, int b) {
+        int t = x[a];
         x[a] = x[b];
         x[b] = t;
+    }
+
+
+    public void mergeSort(int[] array) {
+        int[] src = array.clone();
+        mergeSort(src, array, 0, array.length - 1);
+    }
+
+    private void mergeSort(int[] src, int[] dest, int start, int end) {
+        if (end - start < 2) {
+            if (dest[end] < dest[start]) {
+                swap(dest, start, end);
+            }
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        mergeSort(dest, src, start, mid - 1);
+        mergeSort(dest, src, mid, end);
+
+        if (src[mid - 1] < src[mid]) {
+            // array sorted already
+            System.arraycopy(src, start, dest, start, end - start + 1);
+            return;
+        }
+
+
+        for (int write = start, readLeft = start, readRight = mid; write <= end; write++) {
+            if (readRight > end || (readLeft < mid && src[readLeft] < src[readRight])) {
+                dest[write] = src[readLeft++];
+            } else {
+                dest[write] = src[readRight++];
+            }
+
+        }
+
+
     }
 }
