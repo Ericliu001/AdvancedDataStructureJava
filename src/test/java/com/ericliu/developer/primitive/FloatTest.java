0184a8e7a285d79476e7cf756e5fbaf79840cfec
Eric Liu <eric@loveagency.com.au>
2016-11-03 14:27:50 +1100

    problem with float

---
+++ src/test/java/com/ericliu/developer/primitive/FloatTest.java
@@ -0,0 +1,17 @@
+package com.ericliu.developer.primitive;
+
+import org.junit.Test;
+
+/**
+ * Created by ericliu on 3/11/16.
+ */
+
+public class FloatTest {
+
+
+    @Test
+    public void testFloat(){
+        float a = 1 - 0.9f;
+        System.out.println("Float number a: " + a);
+    }
+}
